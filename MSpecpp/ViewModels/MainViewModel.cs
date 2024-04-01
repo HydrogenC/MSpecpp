ab2ebe0e559f241c0916e592865dd654fdd44d35
ExMatics HydrogenC <33123710+HydrogenC@users.noreply.github.com>
2024-04-01 10:27:50 +0800

    fix build

--- MSpecpp/ViewModels/MainViewModel.cs
+++ MSpecpp/ViewModels/MainViewModel.cs
@@ -8,7 +8,6 @@ using System.Linq;
 using System.Text.Json;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Messaging;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace MSpecpp.ViewModels;
 
